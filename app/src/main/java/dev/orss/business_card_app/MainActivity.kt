647d47c9dd48cbe7efebbb33efa97cbc0521689f
Oscar <orss3214@gmail.com>
2025-06-13 20:44:59 -0400

    added logo, name, and title

--- app/src/main/java/dev/orss/business_card_app/MainActivity.kt
+++ app/src/main/java/dev/orss/business_card_app/MainActivity.kt
@@ -4,13 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import dev.orss.business_card_app.ui.theme.Business_Card_AppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +35,57 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Business_Card_AppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    Business_Card(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun Business_Card_Preview() {
     Business_Card_AppTheme {
-        Greeting("Android")
+        Business_Card()
+    }
+
+}
+
+@Composable
+fun Business_Card(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color(193, 217, 195))
+    ){
+        Column(
+            modifier = Modifier
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.android_logo),
+                contentDescription = "Android Logo",
+                modifier = Modifier
+                    .size(180.dp)
+                    .clip(RoundedCornerShape(8.dp))
+                    .background(Color(0, 43, 17))
+            )
+            Text(
+                text = "Oscar Silva-Santiago",
+                fontSize = 36.sp,
+                fontWeight = FontWeight.Light,
+                color = Color.Black
+            )
+            Text(
+                text = "Android Developer Extraordinaire",
+                fontWeight = FontWeight.Bold,
+                color = Color(0, 89, 35)
+            )
+        }
     }
 }
